ff151730d20af2b23c7dda40852fa6e2ca3bb09d
viowl <sovaworkbox@gmail.com>
2024-02-07 13:16:33 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,9 @@
-import React from "react";
-
 export const App = () => {
   return (
     <>
       <h2>Payments 1</h2>
-      <PaymentList payments={payments1} />
 
       <h2>Payments 2</h2>
-      <PaymentList payments={payments2} />
 
       <h2>All payments</h2>
       <PaymentList payments={payments} />
