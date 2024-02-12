dca431f4753d63b73dd3d396bf8ffddad5af4eb5
viowl <sovaworkbox@gmail.com>
2024-02-12 20:01:15 +0100

    add card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,18 @@
-const Payment = () => {
-  return (
-    <div>
-      <p>Amount: </p>
-      <p>Status: Paid or Pending</p>
-      <p>Description: </p>
-      <p>Card Number: </p>
-      <p>Card Type: </p>
-      <p>Card Holder Name: </p>
-      <p>Payment Date: </p>
-      <hr />
-    </div>
-  );
-};
+import Profile from "./Profile/Profile";
+import user from "../data/user.json";
 
-export const App = () => {
+const App = () => {
   return (
     <>
-      <Payment />
+      <Profile
+        name={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </>
   );
 };
+
+export default App;
