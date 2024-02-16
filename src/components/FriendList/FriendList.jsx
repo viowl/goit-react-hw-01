2248aa607a91ca1066825f46570c9e321f144106
viowl <sovaworkbox@gmail.com>
2024-02-16 13:34:44 +0100

    add ways

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -19,21 +19,38 @@
 // // const FriendList = () => console.log(12);
 
 // // export default FriendList;
-import css from "./FriendList.module.css";
-import { FriendListItem } from "../FriendListItem/FriendListItem";
 
-export const FriendList = ({ friends }) => {
+// import css from "./FriendList.module.css";
+// import FriendListItem from "../FriendListItem/FriendListItem";
+
+// export const FriendList = ({ friends }) => {
+//   return (
+//     <ul className={css.list}>
+//       {friends.map((friend) => (
+//         <li key={friend.id} className={css.listItem}>
+//           <FriendListItem
+//             avatar={friend.avatar}
+//             name={friend.name}
+//             isOnline={friend.isOnline}
+//           />
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+// export default FriendList;
+import FriendListItem from "../FriendListItem//FriendListItem";
+import css from "./FriendList/FriendList.module";
+
+const FriendList = ({ avatar, name, isOnLine, id }) => {
   return (
-    <ul className={css.list}>
-      {friends.map((friend) => (
-        <li key={friend.id} className={css.listItem}>
-          <FriendListItem
-            avatar={friend.avatar}
-            name={friend.name}
-            isOnline={friend.isOnline}
-          />
-        </li>
-      ))}
+    <ul className={css.id}>
+      {/* Кількість li залежить від кількості об'єктів в масиві */}
+      <li>
+        <FriendListItem />
+      </li>
     </ul>
   );
 };
+
+export default FriendList;
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -13,16 +13,21 @@
 
 // export default FriendListItem;
 
-import css from "./FriendListItem.module.css";
-import clsx from "clsx";
+// import css from "./FriendListItem.module.css";
+// import clsx from "clsx";
 
-export const FriendListItem = ({ avatar, name, isOnline }) => {
-  const textColor = clsx(css.textSize, isOnline ? css.isOnline : css.isOffline);
-  return (
-    <div className={css.listItem}>
-      <img src={avatar} alt="Avatar" width="48" />
-      <p className={css.name}>{name}</p>
-      <p className={textColor}>{isOnline ? "Online" : "Offline"}</p>
-    </div>
-  );
-};
+// export const FriendListItem = ({ avatar, name, isOnline }) => {
+//   const textColor = clsx(css.textSize, isOnline ? css.isOnline : css.isOffline);
+//   return (
+//     <div className={css.listItem}>
+//       <img src={avatar} alt="Avatar" width="48" />
+//       <p className={css.name}>{name}</p>
+//       <p className={textColor}>{isOnline ? "Online" : "Offline"}</p>
+//     </div>
+//   );
+// };
+// export default FriendListItem;
+
+const FriendListItem = () => console.log(1111);
+
+export default FriendListItem;
