cb881e92567a3f069af05d0da403bb4654334b08
viowl <sovaworkbox@gmail.com>
2024-02-21 14:07:50 +0100

    finish components
    need add style

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -40,15 +40,16 @@
 // };
 // export default FriendList;
 import FriendListItem from "../FriendListItem//FriendListItem";
-import css from "./FriendList/FriendList.module";
+import css from "./FriendList.module.css";
 
-const FriendList = ({ avatar, name, isOnLine, id }) => {
+const FriendList = ({ friends }) => {
   return (
-    <ul className={css.id}>
-      {/* Кількість li залежить від кількості об'єктів в масиві */}
-      <li>
-        <FriendListItem />
-      </li>
+    <ul className={css.list}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <li key={id} className={css.listItem}>
+          <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+        </li>
+      ))}
     </ul>
   );
 };
