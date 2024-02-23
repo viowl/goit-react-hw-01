a1678bb1cc1c1299747e3f63ff954e96931bd46a
viowl <sovaworkbox@gmail.com>
2024-02-24 00:11:54 +0100

    add style to 2nd test

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -4,8 +4,8 @@ import css from "./FriendListItem.module.css";
 const FriendListItem = ({ avatar, name, isOnline }) => {
   const textColor = clsx(css.textSize, isOnline ? css.isOnline : css.isOffline);
   return (
-    <div className={css.listItem}>
-      <img src={avatar} alt="Avatar" width="48" />
+    <div className={css.listItemContaner}>
+      <img className={css.img} src={avatar} alt="Avatar" width="48" />
       <p className={css.name}>{name}</p>
       <p className={textColor}>{isOnline ? "Online" : "Offline"}</p>
     </div>
